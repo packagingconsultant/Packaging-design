fe6cacdb521e3b1da117ceb5510fd557aa7bba07
packagingconsultant <souravbiswas3107@gmail.com>
2025-06-11 21:43:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, {
+  threshold: 0.4
+});
+
+cards.forEach(card => {
+  observer.observe(card);
+});
